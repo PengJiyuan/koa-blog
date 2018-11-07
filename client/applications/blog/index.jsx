ae6824462a8c2158173b907971b1ac098390a800
PengJiyuan <731401082@qq.com>
2018-11-07 16:30:39 +0800

    update

---
+++ client/applications/blog/index.jsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './style/index.less';
+
+ReactDOM.render(
+  <h1>fasdfsdafasdf</h1>,
+  document.getElementById('container')
+);
---
+++ client/build/webpack.config.js
@@ -0,0 +1,137 @@
+/**
+ * @PengJiyuan
+ */
+const path = require('path');
+const fs = require('fs');
+const autoprefixer = require('autoprefixer');
+const os = require('os');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const lessToJs = require('less-var-parse');
+
+const entry = {};
+fs.readdirSync('../applications')
+  .filter(m => fs.statSync(path.join('../applications', m)).isDirectory())
+  .forEach((m) => {
+    entry[m] = [`../applications/${m}/index.jsx`];
+  });
+
+const themer = lessToJs(fs.readFileSync(path.join(__dirname, '../theme/index.less'), 'utf8'));
+
+const webpackConfig = {
+
+  mode: 'production',
+
+  context: __dirname,
+
+  entry,
+
+  output: {
+    path: path.resolve(__dirname, `../public/`),
+    filename: '[hash:6].[name].min.js'
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }, {
+      test: /\.less|css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins() {
+              return [autoprefixer];
+            }
+          }
+        }, {
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true,
+            sourceMap: true,
+            modifyVars: themer
+          }
+        }
+      ]
+    }, {
+      test: /\.(woff|svg|eot|ttf|otf)\??.*$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          limit: 1000,
+          name: '/fonts/[hash:8].icon.[ext]'
+        }
+      }
+    }]
+  },
+
+  // only show valid/invalid and errors
+  // deal with verbose output
+  stats: {
+    assets: true,
+    colors: true,
+    warnings: true,
+    errors: true,
+    errorDetails: true,
+    entrypoints: true,
+    version: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    children: false
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/]/,
+          name: 'dll',
+          minChunks: 1,
+          maxInitialRequests: 5,
+          minSize: 0,
+          priority: 100
+        }
+      }
+    },
+    minimizer: [
+      new UglifyJSPlugin({
+        parallel: os.cpus().length
+      })
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[hash:6].[name].min.css',
+      chunkFilename: '[id].css'
+    })
+  ],
+
+  resolve: {
+    extensions: ['.jsx', '.js', 'json'],
+    modules: [
+      path.resolve(__dirname, '../'),
+      'node_modules'
+    ]
+  }
+};
+
+// const pluginHtmls = Object.keys(entry).map(id => new HtmlWebpackPlugin({
+//   filename: `${id === 'dashboard' ? 'index' : id}.html`,
+//   inject: true,
+//   template: path.resolve(__dirname, `html/${language}/${id}.html`)
+// }));
+
+// webpackConfig.plugins = webpackConfig.plugins.concat(pluginHtmls);
+
+module.exports = webpackConfig;
