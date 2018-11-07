cc5f671b26d4031282bafefe374ca2f482bd8421
PengJiyuan <731401082@qq.com>
2018-11-07 11:32:05 +0800

    add mysql

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const path = require('path');
 const logger = require('koa-logger');
-const router = require('./routes');
 const koaBody = require('koa-body');
 const views = require('koa-views');
+const router = require('koa-router')();
+const config = require('./config/config');
+const { query } = require('./mysql/query');
 
 const Koa = require('koa');
 const app = module.exports = new Koa();
@@ -18,12 +20,64 @@ app.use(views(path.join(__dirname, '/views'), {
 
 app.use(koaBody());
 
+router.get('/', list);
+router.get('/post/new', add);
+router.get('/post/:id', show);
+router.post('/post', create);
+
+/**
+ * Post listing.
+ */
+
+async function list(ctx) {
+  async function selectAllData() {
+    const sql = 'SELECT * FROM post';
+    return await query(sql);
+  }
+  console.log(await selectAllData())
+  await ctx.render('list', { posts: await selectAllData() });
+}
+
+/**
+ * Show creation form.
+ */
+
+async function add(ctx) {
+  await ctx.render('new');
+}
+
+/**
+ * Show post :id.
+ */
+
+async function show(ctx) {
+  async function getPostById(id) {
+    const sql = `SELECT * FROM post where id=${id}`;
+    return await query(sql);
+  }
+  const id = ctx.params.id;
+  const post = await getPostById(id);
+  await ctx.render('show', { post: post[0] });
+}
+
+/**
+ * Create a post.
+ */
+
+async function create(ctx) {
+  async function insert(data) {
+    const sql = 'INSERT INTO post SET ?';
+    await query(sql, data);
+  }
+  const post = ctx.request.body;
+  post.created_at = new Date();
+  await insert(post);
+  ctx.redirect('/');
+}
+
 app.use(router.routes());
 
 // listen
+if (!module.parent) app.listen(config.port);
 
-const port = 3000;
-
-if (!module.parent) app.listen(port);
-
-console.log(`Listen port ${port}`);
+console.log(`Listen port ${config.port}`);
